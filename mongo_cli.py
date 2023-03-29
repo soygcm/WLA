a87bec8d2b8d7c68a2c6f38974c96279fbc06fc0
Gabriel Nextern <gabriel.castanaza@nextern.com>
2023-03-28 19:26:00 -0600

    now you can insert

--- mongo_cli.py
+++ mongo_cli.py
@@ -7,18 +7,28 @@ import json
 @click.option('--port', default=27017, help='MongoDB port')
 @click.option('--database', required=True, help='MongoDB database name')
 @click.option('--collection', required=True, help='MongoDB collection name')
-@click.option('--query', required=True, help='MongoDB query')
-def mongo_cli(host, port, database, collection, query):
+@click.option('--query', help='MongoDB query')
+@click.option('--insert', help='JSON document to insert')
+def mongo_cli(host, port, database, collection, query, insert):
     client = pymongo.MongoClient(host, port)
     db = client[database]
     collection = db[collection]
-    query = json.loads(query)
-    result = collection.find(query)
-    result_list = list(result)
-    if len(result_list) == 0:
-        print("Vacio")
-    for document in result:
-        print(document)
+    
+    if query:
+        query = json.loads(query)
+        result = collection.find(query)
+        result_list = list(result)
+        if len(result_list) == 0:
+            print("Vacio")
+        for document in result:
+            print(document)
+    elif insert:
+        insert_doc = json.loads(insert)
+        collection.insert_one(insert_doc)
+        print("Documento insertado correctamente")
+    else:
+        print("Debe ingresar una operación (--query o --insert)")
+
     client.close()
 
 if __name__ == '__main__':
